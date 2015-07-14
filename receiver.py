aef1f590bd9d0bcebabcdc209d88c494c28e25cd
Casey Fulton <casey@caseyfulton.com>
2015-07-14 09:48:55 +0000

    Implemented Adafruit PWM Servo Driver library

--- receiver.py
+++ receiver.py
@@ -2,13 +2,22 @@
 # $ pip install bitstring
 
 import socket
+import time
 from bitstring import ConstBitStream
+from Adafruit_PWM_Servo_Driver import PWM
 
-UDP_BIND_IP = "127.0.0.1"
-UDP_PORT = 5040
+
+udp_bind_ip = "127.0.0.1"
+udp_port = 5040
+
+pwm = PWM(0x40)
+servoMin = 150
+servoMax = 600
+travel = servoMax - servoMin
+pwm.setPWMFreq(60)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((UDP_BIND_IP, UDP_PORT))
+s.bind((udp_bind_ip, udp_port))
 
 while True:
     data = s.recv(1024)
@@ -17,8 +26,9 @@ while True:
         continue
 
     bits = ConstBitStream(bytes=data, length=length)
-    print "Received: ", data
-    print "Bits:", bits.bin
     while bits.pos <= length - 16:
         servo, value = bits.readlist('uint:7, int:9')
-        print "Servo: ", servo, "Value: ", value
+        servo_value = int((value + 255) / 512.0 * travel + servoMin)
+        print "Servo:", servo, "Value:", servo_value
+        pwm.setPWM(servo, 0, servo_value)
+        
--- transmitter.py
+++ transmitter.py
@@ -3,7 +3,7 @@
 
 import socket
 from time import sleep
-from bitstring import ConstBitStream
+from bitstring import Bits
 
 UDP_ADDR = "127.0.0.1"
 UDP_PORT = 5040
@@ -11,5 +11,8 @@ UDP_PORT = 5040
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    s.sendto('BZ', (UDP_ADDR, UDP_PORT))
-    sleep(1)
+    for servo_value in range(-255, 256):
+        bits = Bits('uint:7=0, int:9=' + str(servo_value) + ', uint:7=1, int:9=' + str(servo_value))
+        print bits.bin
+        s.sendto(bits.tobytes(), (UDP_ADDR, UDP_PORT))
+        sleep(0.1)
