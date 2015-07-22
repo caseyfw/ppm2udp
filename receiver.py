b4bc85b303659805c2b74847369121697b1918cd
Casey Fulton <casey@caseyfulton.com>
2015-07-22 22:53:52 +0930

    Changed transport layer to 16bit milliseconds.

--- receiver.py
+++ receiver.py
@@ -7,14 +7,11 @@ from bitstring import ConstBitStream
 from lib.Adafruit_PWM_Servo_Driver import PWM
 
 
-udp_bind_ip = "127.0.0.1"
+udp_bind_ip = "0.0.0.0"
 udp_port = 5040
 
 pwm = PWM(0x40)
-servoMin = 150
-servoMax = 600
-travel = servoMax - servoMin
-pwm.setPWMFreq(60)
+pwm.setPWMFreq(1000)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((udp_bind_ip, udp_port))
@@ -22,13 +19,13 @@ s.bind((udp_bind_ip, udp_port))
 while True:
     data = s.recv(1024)
     length = len(data) * 8
-    if length < 16:
+    if length < 16 or length % 16 != 0:
         continue
 
     bits = ConstBitStream(bytes=data, length=length)
+    servo = 0
     while bits.pos <= length - 16:
-        servo, value = bits.readlist('uint:7, int:9')
-        servo_value = int((value + 255) / 512.0 * travel + servoMin)
+        servo_value = bits.read('int:16')
         print "Servo:", servo, "Value:", servo_value
         pwm.setPWM(servo, 0, servo_value)
-
+        servo = servo + 1
---
+++ transmitter_from_serial.py
@@ -0,0 +1,22 @@
+import serial
+import math
+from bitstring import BitArray
+import socket
+
+UDP_ADDR = "127.0.0.1"
+UDP_PORT = 5040
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ser = serial.Serial('/dev/tty.usbmodemfa131', 9600)
+
+# channels = 16
+# servos = [0] * channels
+# min_vals = [1050] * channels
+# max_vals = [1950] * channels
+
+while True:
+	bits = BitArray()
+	line = ser.readline().strip()
+	for pulse_length in line.split():
+		bits.append('uint:16=' + pulse_length)
+	s.sendto(bits.tobytes(), (UDP_ADDR, UDP_PORT))
+	print line
